dfdd60a14d0a1cf633dd21f5cb8e96cda6054659
midhun kalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2024-01-31 17:43:40 +0530

    Initial serer created and the db connection established , the db connection is in the config folder using mongoose and it is require in the main.js

---
+++ config/config.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+require("dotenv").config()
+
+function mongooseConnection(){
+    mongoose.set("strictQuery",true)
+    mongoose.connect(process.env.MONGOOSE_CONNECTION).then(()=>{
+        console.log("db connected")
+    })
+}
+
+module.exports = {
+    mongooseConnection
+}
--- main.js
+++ main.js
@@ -2,8 +2,10 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const session = require('express-session')
-
 const app = express()
+const config = require('./config/config')
+config.mongooseConnection();
+
 const PORT = process.env.PORT || 3000
 
 app.get("/",(req,res)=>{
