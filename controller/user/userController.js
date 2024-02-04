77b793ccea552f1de8c26581cd7de39cc7d943f9
midhun kalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2024-02-04 17:56:56 +0530

    in registration email validation is used in the html and the forgot passwordtext added in login

--- controller/user/userController.js
+++ controller/user/userController.js
@@ -4,7 +4,7 @@ const User = require('../../models/userModel')
 
 const getLogin = async(req,res)=>{
     try{
-        res.render('user/login',{title : "LapShop login"})
+        res.render('user/login',{title : "LapShop login",type : "",message : ""})
     }catch(error){
         console.log(error)
     }
@@ -21,6 +21,11 @@ const getRegister = async(req,res)=>{
 const postRegister = async(req,res)=>{
     try{
 
+        const existingUser = await User.findOne({email : req.body.email})
+        if(existingUser){
+            return res.render('user/registration',{title : "LapShop Register", type : "danger",message : "User already exists"})
+        }
+
         if(req.body.password !== req.body.confirmpassword){
             return res.render('user/registration',{title : "LapShop Register",type : "danger", message : 'Password is not matching'})
         }
@@ -36,11 +41,11 @@ const postRegister = async(req,res)=>{
         const userData = await user.save();
 
         if(userData){
-            return res.render("user/login",{title : "LapShop login"})
             console.log("User added succesfully")
+            return res.render("user/login",{title : "LapShop login",type : "success",message : "Registered successfully"})
         }else{
-            return res.render("user/registration",{title : "LapShop register",type : "",message : ""})
             console.log("User is not added")
+            return res.render("user/registration",{title : "LapShop register",type : "danger",message : error.message})
         }
     }catch(error){
         console.log(error.message)
