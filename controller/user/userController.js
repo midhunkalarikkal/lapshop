d023a8595c8b00bf187bb7242dc3222648a71797
midhun kalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2024-02-07 01:50:14 +0530

    login register otp is done the problems are save user in db before otp validation , it need to be cleared. resend otp button function need to clear. try to use the fetch to send reques from register form data to get otp page and validate then send the otp through email and then validate the otp if successfull then save the user.

--- controller/user/userController.js
+++ controller/user/userController.js
@@ -2,107 +2,200 @@ require('dotenv').config()
 const bcrypt = require('bcrypt')
 const User = require('../../models/userModel')
 const nodemailer = require('nodemailer')
+const crypto = require("crypto")
+const bodyParser = require('body-parser');
+const session = require('express-session')
+const express = require('express')
+const app = express()
 
-const transporter = nodemailer.createTransport({
-    service : "gmail",
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    auth: {
-      user: "lapshopotp@gmail.com",
-      pass: "nyxnnbafpfqznvvh",
-    // user : "blckchainmedicine@gmail.com",
-    // pass : "shggbhblxfkmvbbo"
-    },
-  });
+app.use(session({
+    secret: 'userkey', // Change this to a secure secret key
+    resave: false,
+    saveUninitialized: false
+}));
 
-  const mailOptions = {
-    from: 'lapshopotp@gmail.com', // sender address
-    to: "midhunkalarikkalp@gmail.com", // list of receivers
-    subject: "Hello ✔ from nodemailer", // Subject line
-    text: "Hello world?", // plain text body
-    html: "<b>Hello world?</b>", // html body
-  }
+//for storing otp
+let generatedOtp = "";
 
-  const sendMail = async (transporter, mailOptions)=>{
-    try{
-        await transporter.sendMail(mailOptions)
-        console.log("Email sended succesfullly")
-    }catch(err){
-        console.log(err)
-    }
-  }
-
-  sendMail(transporter, mailOptions)
-
-  const getHome = async(req,res)=>{
-    try{
-        res.render('user/home',{title : "LapShop home"})
-    }catch(error){
-        console.log(error)
-    }
-  }
-
-
-const getLogin = async(req,res)=>{
-    try{
-        res.render('user/login',{title : "LapShop login",type : "",message : ""})
-    }catch(error){
-        console.log(error)
-    }
-}
-
-const getRegister = async(req,res)=>{
-    try{
-        res.render('user/registration',{title : "LapShop Register",type : "",message : ""})
-    }catch(error){
-        console.log(error)
-    }
-}
-
-const postRegister = async(req,res)=>{
-    try{
-        // for if the user is exist or not
-        const existingUser = await User.findOne({email : req.body.email})
-        if(existingUser){
-            return res.render('user/registration',{title : "LapShop Register", type : "danger",message : "User already exists"})
+//for sending email
+const sendVerifyMail = async (name, email, userData) => {
+    try {
+        // Generating OTP
+        function generateOTP() {
+            const crypto = require('crypto'); // Require crypto inside the function
+            const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+            for (let i = 0; i < 6; i++) {
+                const index = crypto.randomInt(0, chars.length);
+                generatedOtp += chars[index];
+            }
+            return generatedOtp;
         }
 
-        const user = new User({
-            fullname:req.body.fullname,
-            email:req.body.email,
-            phone:req.body.phone,
-            password:req.body.password,
-            isblocked:0,
+        const userRegisterOtp = generateOTP();
+        console.log('Generated OTP:', userRegisterOtp);
+
+        //sending otp
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            host: "smtp.gmail.com",
+            port: 465,
+            secure: true,
+            auth: {
+                user: "lapshopotp@gmail.com",
+                pass: "nyxnnbafpfqznvvh",
+            },
         })
 
-        const userData = await user.save();
+        const mailOptions = {
+            from: 'lapshopotp@gmail.com',
+            to: email,
+            subject: "OTP for register in LapShop Ecommerce",
+            text: `Hi ${name} welcome to Lapshop shopping. Here is your Otp ${userRegisterOtp}`,
+            html: `${userRegisterOtp}`
+        };
 
-        if(userData){
-            console.log("User added succesfully")
-            return res.render("user/otpvalidation",{title : "LapShop otp",type : "success",message : "An otp has been sent to your email"})
-        }else{
-            console.log("User is not added")
-            return res.render("user/registration",{title : "LapShop register",type : "danger",message : error.message})
+        transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+                console.log(error)
+            } else {
+                console.log("Email has been sended:-", info.response)
+            }
+        })
+
+        const sendMail = async (transporter, mailOptions) => {
+            try {
+                await transporter.sendMail(mailOptions);
+                userData.save()
+                console.log("Email sent successfully");
+            } catch (err) {
+                console.log(err);
+                throw new Error("Failed to send email");
+            }
+        };
+
+        sendMail(transporter, mailOptions);
+
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+const getHome = async (req, res) => {
+    try {
+        res.render('user/home', { title: "LapShop home" })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+const getLogin = async (req, res) => {
+    try {
+        res.render('user/login', { title: "LapShop login", type: "", message: "" })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const postLogin = async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email });
+        const { email, password } = req.body;
+
+        if (user) {
+            if (user.isblocked) {
+                return res.render("user/login", { title: "LapShop login", type: "danger", message: "Contact us" })
+            }
+
+            //password matching
+            bcrypt.compare(password, user.password, function (err, result) {
+                if (err) {
+                    console.log(err);
+                    return res.status(500).send('An error occurred while comparing the passwords.');
+                } if (result) {
+                    // Passwords match
+                    req.session.user = user;
+                    return res.render("user/home", { title: "LapShop login" })
+                } else {
+                    // Passwords don't match    
+                    return res.render("user/login", { title: "LapShop login", type: "danger", message: "Incorrect password" })
+                }
+            });
+        } else {
+            return res.render("user/login", { title: "LapShop login", type: "danger", message: "No user found" })
         }
-    }catch(error){
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+const getRegister = async (req, res) => {
+    try {
+        res.render('user/registration', { title: "LapShop Register", type: "", message: "" })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const postRegister = async (req, res) => {
+    try {
+        // for if the user is exist or not
+        const existingUser = await User.findOne({ email: req.body.email })
+        if (existingUser) {
+            return res.render('user/registration', { title: "LapShop Register", type: "danger", message: "User already exists" })
+        }
+
+        const hashpassword = await bcrypt.hash(req.body.password, 10)
+
+        const user = new User({
+            fullname: req.body.fullname,
+            email: req.body.email,
+            phone: req.body.phone,
+            password: hashpassword,
+            isblocked: 0,
+        })
+
+        // const userData = await user.save()
+        const userData = user
+
+        if (userData) {
+            console.log("User saved in db")
+            sendVerifyMail(req.body.fullname, req.body.email, userData)
+            return res.render('user/otpvalidation', { title: "LapShop OTP verification", type: "success", message: "Check your email for otp" })
+        } else {
+            return res.render('user/registration', { title: "LapShop register", type: "danger", message: "Registration error" })
+        }
+    } catch (error) {
         console.log(error.message)
         return res.render("user/registration", { title: "LapShop register", type: "danger", message: error.message });
     }
 }
 
-const getOtp = async(req,res)=>{
-    try{
-        res.render('user/otpvalidation',{title : "LapShop otp",type: " ",message : ""})
-    }catch(error){
+const postOtp = async (req, res) => {
+    try {
+        if (generatedOtp === req.body.otp) {
+            generatedOtp = ""
+            return res.render('user/login', { title: "LapShop login", type: "success", message: "Email verified successfullyy" })
+
+        }
+
+    } catch (error) {
         console.log(error.message)
     }
 }
 
-const getNavbar = async(req,res)=>{
-    try{
+const getOtp = async (req, res) => {
+    try {
+        res.render('user/otpvalidation', { title: "LapShop otp", type: "", message: "" })
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+const getNavbar = async (req, res) => {
+    try {
         res.render("user/test")
-    }catch(error){
+    } catch (error) {
         console.log(error.message)
     }
 }
@@ -112,6 +205,8 @@ module.exports = {
     getRegister,
     postRegister,
     getOtp,
+    postOtp,
     getNavbar,
-    getHome
+    getHome,
+    postLogin,
 }
--- routes/user.js
+++ routes/user.js
@@ -11,7 +11,9 @@ userRouter.get('/login',userController.getLogin)
 userRouter.get('/register',userController.getRegister)
 userRouter.post('/register',userController.postRegister)
 userRouter.get('/otp',userController.getOtp)
+userRouter.post('/registerotp',userController.postOtp)
 userRouter.get('/navbar',userController.getNavbar)
 userRouter.get('/home',userController.getHome)
+userRouter.post('/login',userController.postLogin)
 
 module.exports = userRouter
