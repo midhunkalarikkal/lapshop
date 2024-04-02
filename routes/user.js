ce0eae36e67724d325c1637b197377a4ca1c1a42
midhun kalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2024-04-02 13:59:06 +0530

    In user product detail page the zooming functionality is done for all product images

--- routes/user.js
+++ routes/user.js
@@ -93,7 +93,4 @@ userRouter.post('/decrementCartProduct',userAuth.isUserLoggedIn,userController.p
 //To delete a product from cart
 userRouter.post('/deleteProductFromCart',userAuth.isUserLoggedIn,userController.deleteProductFromCart)
 
-// userRouter.post('/shopSearch',userController.getSearchProduct)
-
-
 module.exports = userRouter
