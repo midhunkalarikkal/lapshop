24f3504b1f3fa347e2dfe3fb7eaa4889ec1d057a
midhun kalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2024-04-24 22:15:23 +0530

    In user the wallet model is created

---
+++ models/walletModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+const walletSchema = new mongoose.Schema({
+    user : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "User",
+        required : true
+    },
+    type : {
+        type :  String,
+        enum : ["debit","credit"],
+        required : true
+    },
+    reason : {
+        type : String,
+        required : true
+    },
+    amount : {
+        type : Number,
+        required : true
+    },
+    updatedAt : {
+        type : Date,
+        default : Date.now
+    }
+})
+
+module.exports = mongoose.model("Wallet", walletSchema)
