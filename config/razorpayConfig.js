b920a3e595e6b4be6a73d9c12b69e6fa599a01f7
midhunkalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2025-01-06 17:03:53 +0530

    razorpay configuration updated

--- config/razorpayConfig.js
+++ config/razorpayConfig.js
@@ -1,10 +1,8 @@
 const Razorpay = require("razorpay");
 
-const{KEY_ID, KEY_SECRET}= process.env;
-
 const razorpayInstance =new Razorpay({
-  key_id:KEY_ID,
-  key_secret:KEY_SECRET
+  key_id:process.env.KEY_ID,
+  key_secret:process.env.KEY_SECRET
 })
 
 
