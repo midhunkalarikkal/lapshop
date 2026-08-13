59281ef9c658af4c73281d17b1e2809512a89bbc
midhunkalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2026-08-14 01:53:54 +0530

    fix: mongodb connection error fixed

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+const dns = require("node:dns/promises");
+dns.setServers(["1.1.1.1", "1.0.0.1"]);
+
 const express = require('express');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
